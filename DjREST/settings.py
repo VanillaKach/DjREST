2a5aff8c5e200e4657aa4602b1e1a61a6f2737c6
VanillaKach <4d4m5@mail.ru>
2026-02-05 17:29:19 +0300

    Проект завершен, отправлен на первую проверку

--- DjREST/settings.py
+++ DjREST/settings.py
@@ -58,11 +58,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'DjREST.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Пока используем SQLite
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
 }
+
+# REDIS для Celery
+CELERY_BROKER_URL = config('REDIS_URL')
+CELERY_RESULT_BACKEND = config('REDIS_URL')
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
